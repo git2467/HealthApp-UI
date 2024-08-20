dedd381e330ef81043b4c73d6302f1207b77d25a
1332467 <ncs1332467@gmail.com>
2024-08-20 15:12:50 +0800

    commit

---
+++ src/components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <ul>
+        <li>Home</li>
+        <li>Dashboard</li>
+        <li>Settings</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
